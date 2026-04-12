5c1e94bee47ff9a086ab0e81194f5c4932087f29
Taylor Tamarisk <tamarisktaylor@gmail.com>
2026-04-12 18:03:25 +0100

    commit2

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-frieren-blog-kit.netlify.app/", // Update this to your site URL if needed
+  site: "https://github.com/TamsNotAProgrammer/astro-frieren-blog-kit/", // Update this to your site URL if needed
   markdown: {
     syntaxHighlight: false, // Disable built-in syntax highlighting from Astro
     remarkPlugins: [remarkReadingTime], // Removed rehypePrettyCode
@@ -14,3 +14,4 @@ export default defineConfig({
   integrations: [tailwind(), react(), sitemap()],
   output: "static", // This is fine for a static site
 });
+
